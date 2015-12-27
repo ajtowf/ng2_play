856121785faff6773a27636677d04fc4da128351
ajtowf <ajden.towfeek@gmail.com>
2015-12-27 23:24:04 +0100

    Validation, can't reset the form though

--- app/components/todo/todo.ts
+++ app/components/todo/todo.ts
@@ -1,49 +1,63 @@
 /// <reference path="../../models.ts"/>
 
 import {Component, View} from 'angular2/core';
-import {FORM_DIRECTIVES, FormBuilder, ControlGroup, Control} from 'angular2/common';
+import {FORM_DIRECTIVES, FormBuilder, ControlGroup, Control, AbstractControl} from 'angular2/common';
 import {Validators} from 'angular2/common';
 import {TodoItem} from '../../models';
 
 @Component({
-  selector: 'todo',
-  viewBindings: [FormBuilder]
+    selector: 'todo',
+    viewBindings: [FormBuilder]
 })
 @View({
-  templateUrl: './app/components/todo/todo.html',
-  directives: [FORM_DIRECTIVES]
+    templateUrl: './app/components/todo/todo.html',
+    directives: [FORM_DIRECTIVES]
 })
 export class Todo {
-  todos: Array<TodoItem>;
+    todos: Array<TodoItem>;
 
-  myForm: ControlGroup;
-  newTodo: any;
+    fb: FormBuilder;
+    myForm: ControlGroup;
+    newTodoText: string;
+    newTodoControl: Control;
 
-  constructor(fb: FormBuilder) {
-    this.todos = new Array<TodoItem>();
-    this.todos.push(new TodoItem('Hello world', false));
+    constructor(fb: FormBuilder) {
+        this.fb = fb;
+        this.todos = new Array<TodoItem>();
+        this.todos.push(new TodoItem('Hello world', false));
 
-    this.myForm = fb.group({
-      newTodo: ['', Validators.required]
-    });
-
-    this.newTodo = this.myForm.controls['newTodo'];
-  }
-
-  removeTodo(item: TodoItem) {
-    this.todos.splice(this.todos.indexOf(item), 1);
-  }
-
-  onSubmit() {
-    if (this.myForm.valid) {
-      this.todos.push(new TodoItem(this.newTodo.value, false));
-      this.newTodo.updateValue('');
+        this.buildForm();
     }
-  }
 
-  completeAll() {
-    for (var todo of this.todos) {
-      todo.completed = true;
+    buildForm(): void {
+        this.newTodoControl = new Control('', Validators.required);
+
+        this.myForm = this.fb.group({
+            'newTodo': this.newTodoControl
+        });
     }
-  }
+
+    removeTodo(item: TodoItem) {
+        this.todos.splice(this.todos.indexOf(item), 1);
+    }
+
+    onSubmit(): void {
+        if (this.myForm.valid) {
+            this.todos.push(new TodoItem(this.newTodoText, false));
+
+            // How in hell do I reset this thing and prevent it from being validated?
+            // The only thing that works is rebuilding the whole form/&%¤#""
+            this.buildForm();
+            /*this.newTodoText = '';
+            this.newTodoControl.updateValue('');
+            this.myForm.dirty = false;   
+            this.newTodoControl.dirty = false;*/
+        }
+    }
+
+    completeAll() {
+        for (var todo of this.todos) {
+            todo.completed = true;
+        }
+    }
 }
