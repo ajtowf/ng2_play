9cf9ea0a165e45327a283879d4f18aa75c05e29a
ajtowf <ajden.towfeek@gmail.com>
2016-10-10 22:21:56 +0200

    Re-added about page with links to source etc.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -4,7 +4,32 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
 	selector: 'app-about',
 	template: `
-		Welcome to the about page! This is the ID: {{id}}
+		<md-card>
+   		<h2 md-line>About page</h2>
+   		<p>Router param passed to page is: {{id}}</p>
+			<h2 md-line>Source code on Github</h2>
+			<p>
+				This app is built throughout a series of screencasts, 
+				source code can be found at 
+				<a href="https://github.com/ajtowf/ng2_play/" target="_blank">
+					https://github.com/ajtowf/ng2_play/
+				</a>	
+			</p>
+			<h2 md-line>Youtube</h2>
+			<p>
+				For screencast on the latest and greates tech from web development, check out my youtube channel  
+				<a href="https://www.youtube.com/c/AjdenTowfeek" target="_blank">
+					https://www.youtube.com/c/AjdenTowfeek
+				</a>	
+			</p>
+			<h2 md-line>Twitter</h2>
+			<p>
+				And make sure to follow me on twitter to stay tuned   
+				<a href="https://twitter.com/ajtowf" target="_blank">
+					https://twitter.com/ajtowf
+				</a>	
+			</p>  
+		</md-card>
 	`
 })
 export class AboutComponent implements OnInit {
